57e3f64f7014d957ec565be6e7858a56dcb2815e
Tusshar <tpaul.be21@gmail.com>
2022-10-20 22:37:10 +0530

    Done Routing

--- addtocart/src/components/Header.js
+++ addtocart/src/components/Header.js
@@ -1,22 +1,53 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-const Header=()=>{
-    return(
-        <>
-        <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">E-Commerce</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            
-          </Nav>
-          <i class="fa-solid fa-cart-shopping text-light"></i>
-        </Container>
-      </Navbar>
-        </>
-    )
+import React,{useState} from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Badge from '@mui/material/Badge';
+import {NavLink} from "react-router-dom";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
+  export default function SimpleMenu() {
+    const [anchorEl, setAnchorEl] = useState(null);
+  
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
+  return (
+    <>
+      <Navbar bg="dark" variant="dark"style={{height:"60px"}}>
+        <Container>
+          <NavLink to="/" className="text-decoration-none text-light mx-3">Add to Cart</NavLink>
+          <Nav className="me-auto">
+            <NavLink to="/" className="text-decoration-none text-light">Home</NavLink>
+        
+          </Nav>
+          <Badge badgeContent={4} color="primary"
+          aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}
+          >
+          <i class="fa-solid fa-cart-shopping text-light" style={{fontSize:25,cursor:"pointer"}}></i>
+          </Badge>
+        </Container>
+
+        <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+      </Navbar>
+      
+    </>
+  );
 }
-export default Header
+
