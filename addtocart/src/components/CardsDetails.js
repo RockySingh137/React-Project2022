2697e894ed121b439ad41b75dcbc562ec29394e0
Tusshar <tpaul.be21@gmail.com>
2022-10-21 17:33:03 +0530

    adding selecting items to cart

--- addtocart/src/components/CardsDetails.js
+++ addtocart/src/components/CardsDetails.js
@@ -1,44 +1,104 @@
-import React from 'react'
-import Table from 'react-bootstrap/Navbar';
+import React, { useEffect, useState } from 'react'
+import Table from 'react-bootstrap/Table'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { DLT} from '../redux/actions/action'
+import { ADD } from '../redux/actions/action'
+import {REMOVE}from '../redux/actions/action'
+
 
 const CardsDetails = () => {
+
+  const [data,setData] = useState([]);
+  // console.log(data);
+
+  const {id} = useParams();
+  // console.log(id);
+
+  const history = useNavigate();
+
+  const dispatch = useDispatch();
+
+  
+  const getdata = useSelector((state)=> state.cartreducer.carts);
+  // console.log(getdata);
+
+
+  const compare = ()=>{
+    let comparedata = getdata.filter((e)=>{
+      return e.id == id
+    });
+    setData(comparedata);
+  }
+
+  // add data
+  
+
+  const send = (e)=>{
+    // console.log(e);
+    dispatch(ADD(e));
+  }
+  
+  const dlt = (id)=>{
+    dispatch(DLT(id));
+    history("/");
+}
+
+// remove one
+const remove = (item)=>{
+  dispatch(REMOVE(item))
+}
+
+
+  useEffect(()=>{
+    compare();
+  },[id])
+
   return (
     <>
       <div className="container mt-2">
-        <h2 className='text-center'>Items Detail Page
+        <h2 className='text-center'>Iteams Details Page
         </h2>
 
         <section className='container mt-3'>
           <div className="iteamsdetails">
-            
+          {
+            data.map((ele)=>{
+              return (
+                <>
                 <div className="items_img">
-              <img src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"alt="" />
+              <img src={ele.imgdata} alt="" />
             </div>
 
             <div className="details">
               <Table>
-              <tr>
-              <td>
-                <p> <strong>Restaurant</strong>  :Gulmohar Grand</p>
-                <p> <strong>Price</strong>  : ₹450/-</p>
-                <p> <strong>Dishes</strong>  :Delhi,India</p>
-                <p> <strong>Total</strong>  :₹450/-</p>
-               
+                <tr>
+                  <td>
+                    <p> <strong>Restaurant</strong>  : {ele.rname}</p>
+                    <p> <strong>Price</strong>  : ₹{ele.price}</p>
+                    <p> <strong>Dishes</strong>  : {ele.address}</p>
+                    <p> <strong>Total</strong>  :₹  {ele.price * ele.qnty}</p>
+                    <div className='mt-5 d-flex justify-content-between align-items-center' style={{width:100,cursor:"pointer",background:"#ddd",color:"#111"}}>
+                    <span style={{fontSize:24}} onClick={ele.qnty <=1 ? ()=>dlt(ele.id) : ()=>remove(ele)}>-</span>
+                    <span style={{fontSize:22}}>{ele.qnty}</span>
+                    <span style={{fontSize:24}} onClick={()=>send(ele)}>+</span>
 
-              </td>
-              <td>
-                <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}>5 ★	</span></p>
-                <p><strong>Order Review :</strong> <span >Good	</span></p>
-                <p><strong>Remove :</strong> <span ><i className='fas fa-trash' style={{color:"red",fontSize:20,cursor:"pointer"}}></i></span></p>
+                    </div>
 
-                
-              </td>
-            </tr>
+                  </td>
+                  <td>
+                    <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}>{ele.rating} ★	</span></p>
+                    <p><strong>Order Review :</strong> <span >{ele.somedata}	</span></p>
+                    <p><strong>Remove :</strong> <span ><i className='fas fa-trash' onClick={()=>dlt(ele.id)} style={{color:"red",fontSize:20,cursor:"pointer"}}></i>	</span></p>
+                  </td>
+                </tr>
               </Table>
             </div>
           
-               
-          
+                </>
+              )
+            })
+          }
           </div>
         </section>
       </div>
