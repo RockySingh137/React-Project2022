d6f1c3e0c5a0607ffa1292095223760e7d843a03
Tusshar <tpaul.be21@gmail.com>
2022-10-21 12:18:15 +0530

    UI modified

--- addtocart/src/components/Cards.js
+++ addtocart/src/components/Cards.js
@@ -2,39 +2,40 @@ import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Cardsdata from './CardsData';
+import "./style.css";
 
 const Cards = () => {
-  const [data,setData]=useState(Cardsdata);
+  const [data, setData] = useState(Cardsdata);
   //console.log(data);
   return (
     <div className='container mt-3'>
-    <h2 className='text-center'>Add to Cart Project</h2>
-    <div className="row">
-    {
-      data.map((element,id)=()=>{
-        return(
-          <>
-          
-          </>
-        )
-      })
-    }
-    
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </div>
-    </div>
+      <h2 className='text-center'>Add to Cart Projects</h2>
 
+      <div className="row d-flex justify-content-center align-items-center">
+        {
+          data.map((element, id) => {
+            return (
+              <>
+              <Card style={{ width: '22rem',border:"none" }} className="mx-2 mt-4 card_style">
+              <Card.Img variant="top" src={element.imgdata} style={{height:"14rem"}} className="mt-3"/>
+              <Card.Body>
+                <Card.Title>{element.rname}</Card.Title>
+                <Card.Text>
+                  Price: ₹{element.price}/-
+                </Card.Text>
+                <div className="button_div d-flex justify-content-center">
+                <Button variant="primary" className='col-lg-12'>Add to Cart</Button>
+                </div>
+                
+              </Card.Body>
+            </Card>
+              </>
+            )
+          })
+        }
+
+      </div>
+    </div>
   )
 }
-
 export default Cards
--- addtocart/src/components/CardsDetails.js
+++ addtocart/src/components/CardsDetails.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import Table from 'react-bootstrap/Navbar';
 
 const CardsDetails = () => {
   return (
-    <div>CardsDetails</div>
+    <>
+      <div className="container mt-2">
+        <h2 className='text-center'>Iteams Details Page
+        </h2>
+
+        <section className='container mt-3'>
+          <div className="iteamsdetails">
+            
+                <div className="items_img">
+              <img src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"alt="" />
+            </div>
+
+            <div className="details">
+              <Table>
+              <tr>
+              <td>
+                <p> <strong>Restaurant</strong>  :Gulmohar Grand</p>
+                <p> <strong>Price</strong>  : ₹450/-</p>
+                <p> <strong>Dishes</strong>  :Delhi,India</p>
+                <p> <strong>Total</strong>  :₹450/-</p>
+               
+
+              </td>
+              <td>
+                <p><strong>Rating :</strong> <span style={{background:"green",color:"#fff",padding:"2px 5px",borderRadius:"5px"}}>5 ★	</span></p>
+                <p><strong>Order Review :</strong> <span >Good	</span></p>
+                
+              </td>
+            </tr>
+              </Table>
+            </div>
+          
+               
+          
+          </div>
+        </section>
+      </div>
+    </>
   )
 }
 
